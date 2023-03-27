8963e6f8b8a1c25e2b7373c90992b4d5f9f5830c
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-03-27 22:43:36 +0200

    Add NavigationView, background and timer button in ContentView

--- ClockIn/ContentView.swift
+++ ClockIn/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
+    @State var timer: String = "8:00"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                LinearGradient(colors: [.blue, colorScheme == .light ? .white : .black], startPoint: .topLeading, endPoint: .bottomTrailing)
+                    .ignoresSafeArea()
+                VStack {
+                    Button {
+                        //start timer here
+                    } label: {
+                        Text(timer)
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                            .background {
+                                Circle()
+                                    .frame(width: 200,height: 200)
+                            }
+                    }
+
+                }
+                
+            }
+            .navigationTitle("ClockIn")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                NavigationLink {
+                    Text("History screen")
+                } label: {
+                        Text("History")
+                    }
+                }
+            }
+            
+            
         }
-        .padding()
     }
 }
 
