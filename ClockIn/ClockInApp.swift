1ad02b4acbb61d7890e05f81f35a4b4e62c86564
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-03-27 21:42:28 +0200

    Initial Commit

---
+++ ClockIn/ClockInApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockInApp.swift
+//  ClockIn
+//
+//  Created by Tomasz Kubiak on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ClockInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
