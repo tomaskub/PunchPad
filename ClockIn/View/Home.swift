a8f84c66575a7dea10a041c06d3d53e5771f8626
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-03-28 19:50:33 +0200

    Modify the rings in Home view Add logic to control rings progress in TimerModel

--- ClockIn/View/Home.swift
+++ ClockIn/View/Home.swift
@@ -22,27 +22,59 @@ struct Home: View {
                 //Foreground layer
                 GeometryReader { proxy in
                     VStack {
+                        Spacer()
                         ZStack {
                             Circle()
-                                .trim(from: 0, to: timer.progress)
-                                .stroke(Color.purple.opacity(0.7), lineWidth: 10)
-                                .rotationEffect(.init(degrees: -90))
-                                .onReceive(Timer.publish(every: 1, on: .main, in: .common).autoconnect()) { _ in
-                                    if timer.isStarted {
-                                        timer.updateTimer()
-                                    }
-                                }
-                            Text("\(timer.seconds):")
+                                .fill(.white.opacity(0.3))
+                                .padding()
+                            
+                            hourRing
+                            
+                            Text("\(timer.timerStringValue)")
+                        }
+                        .padding(60)
+                        .onReceive(Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()) { _ in
+                            if timer.isStarted {
+                                timer.updateTimer()
+                            }
                         }
                         Spacer()
-                        Button(action: timer.startTimer) {
-                            Image(systemName: "play.fill")
+                        Button(action:
+                                timer.isStarted ? timer.stopTimer : timer.startTimer) {
+                            Image(systemName: timer.isStarted ? "pause.fill" : "play.fill")
                                 .font(.largeTitle)
                                 .foregroundColor(.white)
                         }
                         Spacer()
+                        HStack {
+                            
+                            Picker("hours", selection: $timer.hours) {
+                                ForEach(0..<25) { i in
+                                    Text("\(i)").tag(i)
+                                        .foregroundColor(.black)
+                                }
+                            }
+                            .pickerStyle(.wheel)
+                            Picker("minutes", selection: $timer.minutes) {
+                                ForEach(0..<60) { i in
+                                    Text("\(i)").tag(i)
+                                        .foregroundColor(.black)
+                                }
+                            }
+                            .pickerStyle(.wheel)
+                            Picker("seconds", selection: $timer.seconds) {
+                                ForEach(0..<60) { i in
+                                    Text("\(i)").tag(i)
+                                        .foregroundColor(.black)
+                                }
+                            }
+                            .pickerStyle(.wheel)
+                        }
+                        .frame(height: 150)
+                        .frame(maxWidth: .infinity)
+                        .background(.white)
+                        .cornerRadius(10)
                     }
-                    .padding(60)
                     .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
                 }
                 .padding()
@@ -61,7 +93,7 @@ struct Home: View {
                 }
                 ToolbarItem(placement: .navigationBarTrailing) {
                     NavigationLink {
-                        Text("Settings")
+                        SettingsView().navigationTitle("Settings")
                     } label: {
                         Text("Settings")
                     }
@@ -71,6 +103,39 @@ struct Home: View {
             
         }
     }
+    
+    var secondRing: some View {
+        Circle()
+            .trim(from: (timer.secondProgress) >= 0.05 ? timer.secondProgress - 0.05 : 0, to: timer.secondProgress)
+            .stroke(Color.purple.opacity(0.7), lineWidth: 10)
+            .rotationEffect(.init(degrees: -90))
+//            .onReceive(Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()) { _ in
+//                if timer.isStarted {
+//                    timer.updateTimer()
+//                }
+//            }
+    }
+    var minuteRing: some View {
+        Circle()
+            .trim(from: 0, to: timer.minuteProgress)
+            .stroke(Color.pink.opacity(0.7), lineWidth: 10)
+            .rotationEffect(.init(degrees: -90))
+            .padding(-20)
+//            .onReceive(Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()) { _ in
+//                if timer.isStarted {
+//                    timer.updateTimer()
+//                }
+//            }
+    }
+    var hourRing: some View {
+        Circle()
+            .trim(from: 0, to: timer.progress)
+            .stroke(Color.black.opacity(0.7), lineWidth: 10)
+            .rotationEffect(.init(degrees: -90))
+            .padding(-40)
+            
+    }
+    
 }
 
 struct Home_Previews: PreviewProvider {
--- ClockIn/ViewModel/TimerModel.swift
+++ ClockIn/ViewModel/TimerModel.swift
@@ -9,26 +9,45 @@ import SwiftUI
 
 class TimerModel: NSObject, ObservableObject {
     //MARK: TIMER PROPERTIES
-    @Published var progress: CGFloat = 0.6
+    
+    @Published var progress: CGFloat = 0.5
+    
+    @Published var secondProgress: CGFloat = 0.0
+    @Published var minuteProgress: CGFloat = 0.0
+    
     @Published var timerStringValue: String = "00:00"
     @Published var isStarted: Bool = false
     @Published var addNewTimer: Bool = false
     
-    @Published var hours: Int = 0
-    @Published var minutes: Int = 0
-    @Published var seconds: Int = 60
+    @Published var hours: Int = 1
+    @Published var minutes: Int = 10
+    @Published var seconds: Int = 0
     
     private var totalSeconds: Int = 0
     
+    private var currentHours: Int {
+        return totalSeconds/3600
+    }
+    
+    private var currentMinutes: Int {
+        return (totalSeconds % 3600) / 60
+    }
+    
+    private var currentSeconds: Int {
+        return (totalSeconds % 60)
+    }
     
     func startTimer()  {
         isStarted = true
         progress = 0
-        totalSeconds = seconds
+        totalSeconds = (hours * 3600) + (minutes * 60) + seconds
         
     }
     
     func stopTimer() {
+        hours = 0
+        minutes = 0
+        seconds = 0
         totalSeconds = 0
         progress = 0
         isStarted = false
@@ -36,13 +55,44 @@ class TimerModel: NSObject, ObservableObject {
     
     func updateTimer() {
         if isStarted {
-            if seconds > 0 {
-                seconds -= 1
-                progress = CGFloat(seconds) / CGFloat(totalSeconds)
+            if totalSeconds >= 0 {
+            
+                totalSeconds -= 1
                 
-            } else if seconds == 0 {
+                updateSecondProgress()
+                updateMinuteProgres()
+                
+                withAnimation(.easeInOut) {
+                    progress = 1 - CGFloat(totalSeconds) / CGFloat((hours * 3600) + (minutes * 60) + seconds)
+                }
+            
+            } else if totalSeconds < 0 {
                 stopTimer()
+//                startTimer()
             }
+        }
+        updateTimerStringValue()
+    }
+    
+    func updateSecondProgress() {
+        withAnimation(.linear) {
+            secondProgress = 1 - (CGFloat(currentSeconds) / CGFloat(60))
+        }
+    }
+    func updateMinuteProgres() {
+        withAnimation(.easeInOut) {
+            minuteProgress = 1 - (CGFloat(currentMinutes) / CGFloat(60))
+        }
+    }
+    func updateTimerStringValue() {
+        let hoursComponent: String = currentHours < 10 ? "0\(currentHours)" : "\(currentHours)"
+        let minutesComponent: String = currentMinutes < 10 ? "0\(currentMinutes)" : "\(currentMinutes)"
+        let secondsComponent: String = currentSeconds < 10 ? "0\(currentSeconds)" : "\(currentSeconds)"
+        
+        if currentHours > 0 {
+            timerStringValue = "\(hoursComponent) : \(minutesComponent)"
+        } else {
+            timerStringValue = "\(minutesComponent) : \(secondsComponent)"
         }
     }
 }
