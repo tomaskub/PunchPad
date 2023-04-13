b9409961758c56667f1466c12196ffebf360292d
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-04-13 22:13:26 +0200

    TimerModelTests: - Add tests for updating the timer: when updating with value higher than timer total - no overtime allowed and overtime allowed

--- ClockInTests/TimerModelTests.swift
+++ ClockInTests/TimerModelTests.swift
@@ -33,7 +33,36 @@ final class TimerModelTests: XCTestCase {
         let valuePredictedAfterOneFire: CGFloat = 1 - CGFloat(sut.timerTotalSeconds - 10) / CGFloat (sut.timerTotalSeconds)
         XCTAssertEqual(sut.progress, valuePredictedAfterOneFire, "Timer should fire once")
     }
-
+    
+    func testUpdateTimer_whenCountSecondsBiggerThanSubtrahendAndNoOvertimeAllowed() throws {
+        sut.progressAfterFinish = false
+        sut.startTimer()
+        
+        for _ in 0...sut.timerTotalSeconds - 1 {
+            
+                MockTimer.currentTimer.fire()
+            
+        }
+//        XCTAssertFalse(MockTimer.currentTimer.isValid, "Timer should be invalid")
+        XCTAssertEqual(sut.progress, 1, "Progress should be equal to 1")
+    }
+    
+    func testUpdateTimer_whenCountSecondsBiggerThanSubtrahendAndOvertimeAllowed() throws {
+        sut.progressAfterFinish = true
+        sut.startTimer()
+        
+        for _ in 0...sut.timerTotalSeconds + 9  {
+            
+                MockTimer.currentTimer.fire()
+            
+        }
+        let valuePredicted: CGFloat = CGFloat(10) / CGFloat (sut.overtimeTotalSeconds)
+//        XCTAssertTrue(MockTimer.currentTimer.isValid, "Timer should be valid")
+        XCTAssertEqual(sut.progress, 1, "Progress should be equal to 1")
+        XCTAssertEqual(sut.overtimeProgress, valuePredicted, "overtimeProgress should be equal to predicted value")
+        
+    }
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
