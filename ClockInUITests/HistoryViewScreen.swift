fd35711b6a555040e53c73800af4d69d4b5041b7
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-09-08 18:58:14 +0200

    Add properties to HistoryViewScreen

--- ClockInUITests/HistoryViewScreen.swift
+++ ClockInUITests/HistoryViewScreen.swift
@@ -9,12 +9,26 @@ import XCTest
 
 final class HistoryViewScreen {
     
-    private typealias Identifier = ScreenIdentifier.OnboardingView
+    private typealias Identifier = ScreenIdentifier.HistoryView
     private let app: XCUIApplication
     
     init(app: XCUIApplication) {
         self.app = app
     }
     
+    var addEntryButton: XCUIElement {
+        app.buttons[Identifier.addEntryButton.rawValue]
+    }
     
+    var entryRows: XCUIElement {
+        app.cells[Identifier.entryRow.rawValue]
+    }
+    
+    var deleteEntryButton: XCUIElement {
+        app.cells.buttons[Identifier.deleteEntryButton.rawValue]
+    }
+    
+    var editEntryButton: XCUIElement {
+        app.cells.buttons[Identifier.editEntryButton.rawValue]
+    }
 }
