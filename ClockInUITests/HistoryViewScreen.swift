82685e4e118302f1458ec0f842bfc0d07b64278f
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-09-08 18:44:24 +0200

    Initial add of HistoryViewScreen

---
+++ ClockInUITests/HistoryViewScreen.swift
@@ -0,0 +1,20 @@
+//
+//  HistoryViewScreen.swift
+//  ClockInUITests
+//
+//  Created by Tomasz Kubiak on 08/09/2023.
+//
+
+import XCTest
+
+final class HistoryViewScreen {
+    
+    private typealias Identifier = ScreenIdentifier.OnboardingView
+    private let app: XCUIApplication
+    
+    init(app: XCUIApplication) {
+        self.app = app
+    }
+    
+    
+}
