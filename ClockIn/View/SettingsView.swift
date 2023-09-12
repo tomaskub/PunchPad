25c5abfe7e546b9d87afbef177d4ee871cfcf2a6
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-09-12 22:58:50 +0200

    Change nav stack to nav view in preview Remove nav title from preview

--- ClockIn/View/SettingsView.swift
+++ ClockIn/View/SettingsView.swift
@@ -198,8 +198,8 @@ struct SettingsView: View {
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationStack {
-            SettingsView().navigationTitle("Settings")
+        NavigationView {
+            SettingsView()
         }
     }
         
