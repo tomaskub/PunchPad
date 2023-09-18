07836afe165916cf59eef8f4baa640e54bb14d3f
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-09-18 20:44:21 +0200

    Move subscribers to published values to combine Add func clearStore

--- ClockIn/Model/SettingsStore.swift
+++ ClockIn/Model/SettingsStore.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
+import Combine
 
 final class SettingsStore: ObservableObject {
-    // setting keys - not sure if enum here is what i need for keys
+    private var subscriptions = Set<AnyCancellable>()
+    private let defaults = UserDefaults.standard
+    
     enum SettingKey: String, CaseIterable {
         case isRunFirstTime
         case isLoggingOvertime
@@ -20,55 +23,14 @@ final class SettingsStore: ObservableObject {
         case savedColorScheme
     }
     
-    private let defaults = UserDefaults.standard
-    
-    @Published var isRunFirstTime: Bool {
-        didSet {
-            updateSetting(setting: .isRunFirstTime, value: isRunFirstTime)
-        }
-    }
-    @Published var isLoggingOvertime: Bool {
-        didSet {
-            updateSetting(setting: .isLoggingOvertime, value: isLoggingOvertime)
-        }
-    }
-    @Published var isCalculatingNetPay: Bool {
-        didSet {
-            updateSetting(setting: .isCalculatingNetPay, value: isCalculatingNetPay)
-        }
-    }
-    
-    @Published var isSendingNotification: Bool {
-        didSet {
-            updateSetting(setting: .isSendingNotifications, value: isSendingNotification)
-        }
-    }
-    
-    @Published var maximumOvertimeAllowedInSeconds: Int {
-        didSet {
-            updateSetting(setting: .maximumOvertimeAllowedInSeconds, value: maximumOvertimeAllowedInSeconds)
-        }
-    }
-    
-    @Published var workTimeInSeconds: Int {
-        didSet {
-            updateSetting(setting: .workTimeInSeconds, value: workTimeInSeconds)
-        }
-    }
-    
-    @Published var grossPayPerMonth: Int {
-        didSet {
-            updateSetting(setting: .grossPayPerMonth, value: grossPayPerMonth)
-        }
-    }
-    
-    @Published var savedColorScheme: ColorScheme? {
-        didSet {
-            if let rawValue = savedColorScheme?.rawValue {
-                updateSetting(setting: .savedColorScheme, value: rawValue)
-            }
-        }
-    }
+    @Published var isRunFirstTime: Bool
+    @Published var isLoggingOvertime: Bool
+    @Published var isCalculatingNetPay: Bool
+    @Published var isSendingNotification: Bool
+    @Published var maximumOvertimeAllowedInSeconds: Int
+    @Published var workTimeInSeconds: Int
+    @Published var grossPayPerMonth: Int
+    @Published var savedColorScheme: ColorScheme?
     
     init() {
         if let value = defaults.value(forKey: SettingKey.isRunFirstTime.rawValue) as? Bool {
@@ -83,6 +45,78 @@ final class SettingsStore: ObservableObject {
         self.workTimeInSeconds = defaults.integer(forKey: SettingKey.workTimeInSeconds.rawValue)
         self.grossPayPerMonth = defaults.integer(forKey: SettingKey.grossPayPerMonth.rawValue)
         self.savedColorScheme = ColorScheme.fromStringValue(defaults.string(forKey: SettingKey.savedColorScheme.rawValue))
+        self.setUpSubscribersSavingToUserDefaults()
+    }
+    
+    func setUpSubscribersSavingToUserDefaults() {
+        $isRunFirstTime
+            .removeDuplicates()
+            .sink { [weak self] value in
+                guard let self else { return }
+                self.updateSetting(setting: .isRunFirstTime, value: value)
+            }.store(in: &subscriptions)
+        $isLoggingOvertime
+            .removeDuplicates()
+            .sink { [weak self] value in
+                guard let self else { return }
+                self.updateSetting(setting: .isLoggingOvertime, value: value)
+            }.store(in: &subscriptions)
+        $isCalculatingNetPay
+            .removeDuplicates()
+            .sink { [weak self] value in
+                guard let self else { return }
+                self.updateSetting(setting: .isCalculatingNetPay, value: value)
+            }.store(in: &subscriptions)
+        $isSendingNotification
+            .removeDuplicates()
+            .sink { [weak self] value in
+                guard let self else { return }
+                self.updateSetting(setting: .isSendingNotifications, value: value)
+            }.store(in: &subscriptions)
+     $maximumOvertimeAllowedInSeconds
+            .removeDuplicates()
+            .sink { [weak self] value in
+                guard let self else { return }
+                self.updateSetting(setting: .maximumOvertimeAllowedInSeconds, value: value)
+            }.store(in: &subscriptions)
+        $workTimeInSeconds
+            .removeDuplicates()
+            .sink { [weak self] value in
+                guard let self else { return }
+                self.updateSetting(setting: .workTimeInSeconds, value: value)
+            }.store(in: &subscriptions)
+        $grossPayPerMonth
+            .removeDuplicates()
+            .sink { [weak self] value in
+                guard let self else { return }
+                self.updateSetting(setting: .grossPayPerMonth, value: value)
+            }.store(in: &subscriptions)
+        $savedColorScheme
+            .removeDuplicates()
+            .sink { [weak self] value  in
+                guard let self else { return }
+                self.updateSetting(setting: .savedColorScheme, value: value)
+            }.store(in: &subscriptions)
+    }
+    
+    func clearStore() {
+        subscriptions.removeAll()
+        for key in SettingKey.allCases {
+            UserDefaults.standard.removeObject(forKey: key.rawValue)
+        }
+        if let value = defaults.value(forKey: SettingKey.isRunFirstTime.rawValue) as? Bool {
+            self.isRunFirstTime = value
+        } else {
+            self.isRunFirstTime = true
+        }
+        isLoggingOvertime = defaults.bool(forKey: SettingKey.isLoggingOvertime.rawValue)
+        isCalculatingNetPay = defaults.bool(forKey: SettingKey.isCalculatingNetPay.rawValue)
+        isSendingNotification = defaults.bool(forKey: SettingKey.isSendingNotifications.rawValue)
+        maximumOvertimeAllowedInSeconds = defaults.integer(forKey: SettingKey.maximumOvertimeAllowedInSeconds.rawValue)
+        workTimeInSeconds = defaults.integer(forKey: SettingKey.workTimeInSeconds.rawValue)
+        grossPayPerMonth = defaults.integer(forKey: SettingKey.grossPayPerMonth.rawValue)
+        savedColorScheme = ColorScheme.fromStringValue(defaults.string(forKey: SettingKey.savedColorScheme.rawValue))
+        setUpSubscribersSavingToUserDefaults()
     }
     
     static func clearUserDefaults() {
