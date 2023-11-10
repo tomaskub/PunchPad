5122a2a8dafe2886e4da1b65bb01370aa55f91c2
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-11-10 22:55:34 +0100

    Refactor StatisticsView

--- ClockIn/View/StatisticsView.swift
+++ ClockIn/View/StatisticsView.swift
@@ -14,14 +14,30 @@ struct StatisticsView: View {
     @Environment(\.colorScheme) var colorScheme
     @EnvironmentObject var container: Container
     @StateObject private var viewModel: StatisticsViewModel
+    @State var chartType: ChartType = .time
+    
+    let navTitleText: String = "Statistics"
+    let salaryCalculationHeaderText: String = "Salary calculation"
+    var chartTitle: String {
+        // this is related to view not view model?
+        switch viewModel.chartType {
+        case .finishTime:
+            return "Time finished"
+        case .startTime:
+            return "Time started"
+        case .time:
+            return "Time worked"
+        }
+    }
+    
     init(viewModel: StatisticsViewModel) {
         self._viewModel = StateObject(wrappedValue: viewModel)
     }
+    
     //MARK: VIEW BODY
     var body: some View {
         ZStack {
-            // BACKGROUND LAYER
-            BackgroundFactory.buildGradient(colorScheme: colorScheme)
+            background
             // CONTENT LAYER
             List {
                 Section {
@@ -29,10 +45,8 @@ struct StatisticsView: View {
                         .padding(.top, 16)
                     chartTypePicker
                 } header: {
-                    Text(chartTitle)
+                    sectionHeader(chartTitle)
                         .accessibilityIdentifier(Identifier.SectionHeaders.chart.rawValue)
-                        .bold()
-                        .foregroundColor(.primary)
                 }//END OF SECTION
                 
                 Section {
@@ -53,41 +67,64 @@ struct StatisticsView: View {
                         } // END OF HSTACK
                     } // END OF FOR EACH
                 } header: {
-                    Text("Salary calculation")
+                    sectionHeader(salaryCalculationHeaderText)
                         .accessibilityIdentifier(Identifier.SectionHeaders.salaryCalculation.rawValue)
                 }// END OF SECTION
             } //END OF LIST
             .scrollContentBackground(.hidden)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    NavigationLink {
-                        HistoryView(viewModel:
-                                        HistoryViewModel(
-                                            dataManager: container.dataManager,
-                                            settingsStore: container.settingsStore)
-                        )
-                    } label: {
-                        Text("Detailed history")
-                    } // END OF NAV LINK
-                    .accessibilityIdentifier(Identifier.NavigationBarButtons.detailedHistory.rawValue)
-                } // END OF TOOLBAR ITEM
-            } // END OF TOOLBAR
-            .navigationTitle("Statistics")
+            .toolbar { toolbar }
+            .navigationTitle(navTitleText)
         } // END OF ZSTACK
     } //END OF VIEW
-    
-    //MARK: UI ELEMENTS
-    var chartTitle: String {
-        switch viewModel.chartType {
-        case .finishTime:
-            return "Time finished"
-        case .startTime:
-            return "Time started"
-        case .time:
-            return "Time worked"
+}
+
+//MARK: AUX. UI ELEMENTS
+extension StatisticsView {
+    var chartTypePicker: some View {
+        Picker("Chart type", selection: $viewModel.chartType) {
+            Text("Time")
+                .tag(ChartType.time)
+                .accessibilityIdentifier(Identifier.ChartTypeButton.workTime.rawValue)
+            Text("Start time")
+                .tag(ChartType.startTime)
+                .accessibilityIdentifier(Identifier.ChartTypeButton.startTime.rawValue)
+            Text("Finish time")
+                .tag(ChartType.finishTime)
+                .accessibilityIdentifier(Identifier.ChartTypeButton.finishTime.rawValue)
         }
+        .accessibilityIdentifier(Identifier.SegmentedControl.chartType.rawValue)
+        .pickerStyle(.segmented)
     } // END OF VAR
+    var background: some View {
+        BackgroundFactory.buildSolidColor()
+    }
     
+    var toolbar: some ToolbarContent {
+        ToolbarItem(placement: .navigationBarTrailing) {
+            //TODO: REPLACE WITH SETTINGS LINK - settings link does not work for some reason?
+            NavigationLink {
+                HistoryView(viewModel:
+                                HistoryViewModel(
+                                    dataManager: container.dataManager,
+                                    settingsStore: container.settingsStore)
+                )
+            } label: {
+                Label("Settings", systemImage: "gearshape.fill")
+            } // END OF NAV LINK
+            .tint(.primary)
+            .accessibilityIdentifier(Identifier.NavigationBarButtons.detailedHistory.rawValue)
+        } // END OF TOOLBAR ITEM
+    }
+    
+    @ViewBuilder
+    func sectionHeader(_ text: String) -> some View {
+        Text(text)
+            .bold()
+            .foregroundColor(.primary)
+    }
+}
+
+extension StatisticsView {
     @ViewBuilder
     var chart: some View {
         switch viewModel.chartType {
@@ -122,9 +159,45 @@ struct StatisticsView: View {
         .accessibilityIdentifier(Identifier.Chart.workTimeChart.rawValue)
         //Additional chart properties x-axis and y-scale
         .chartYScale(domain: 0...15)
-         
+        
     } // END OF VAR
     
+    var chartStartTime: some View {
+        Chart(viewModel.entriesForChart) {
+            PointMark(
+                x: .value("Date", $0.startDate),
+                y: .value("Date", Calendar.current.dateComponents([.hour, .minute], from:  $0.startDate).hour!)
+            )
+            .foregroundStyle($0.workTimeInSeconds == 0 ? .clear : .blue)
+        }
+        .accessibilityIdentifier(Identifier.Chart.startTimeChart.rawValue)
+        .chartYScale(domain: 0...24)
+    } // END OF VAR
+    var chartFinishTime: some View {
+        Chart(viewModel.entriesForChart) {
+            PointMark(
+                x: .value("Date", $0.startDate),
+                y: .value("Date", Calendar.current.dateComponents([.hour, .minute], from:  $0.finishDate).hour!)
+            )
+            .foregroundStyle($0.workTimeInSeconds == 0 ? .clear : .red)
+        }
+        .accessibilityIdentifier(Identifier.Chart.finishTimeChart.rawValue)
+        .chartYScale(domain: 0...24)
+    } // END OF VAR
+}
+
+extension StatisticsView {
+    var chartStartTimeLegend: some View {
+        HStack {
+            Text("Legend:")
+            Circle()
+                .fill(.blue)
+                .frame(width: 14, height: 14)
+            Text("Clock in")
+        }
+        .accessibilityIdentifier(Identifier.ChartLegend.startTimeChartLegend.rawValue)
+        .font(.caption)
+    } // END OF VAR
     var chartWorkTimeLegend: some View {
         HStack {
             Text("Legend:")
@@ -140,43 +213,6 @@ struct StatisticsView: View {
         .accessibilityIdentifier(Identifier.ChartLegend.workTimeChartLegend.rawValue)
         .font(.caption)
     } // END OF VAR
-    
-    var chartStartTime: some View {
-        Chart(viewModel.entriesForChart) {
-                PointMark(
-                    x: .value("Date", $0.startDate),
-                    y: .value("Date", Calendar.current.dateComponents([.hour, .minute], from:  $0.startDate).hour!)
-                )
-                .foregroundStyle($0.workTimeInSeconds == 0 ? .clear : .blue)
-        }
-        .accessibilityIdentifier(Identifier.Chart.startTimeChart.rawValue)
-        .chartYScale(domain: 0...24)
-    } // END OF VAR
-    
-    var chartStartTimeLegend: some View {
-        HStack {
-            Text("Legend:")
-            Circle()
-                .fill(.blue)
-                .frame(width: 14, height: 14)
-            Text("Clock in")
-        }
-        .accessibilityIdentifier(Identifier.ChartLegend.startTimeChartLegend.rawValue)
-        .font(.caption)
-    } // END OF VAR
-    
-    var chartFinishTime: some View {
-        Chart(viewModel.entriesForChart) {
-                PointMark(
-                    x: .value("Date", $0.startDate),
-                    y: .value("Date", Calendar.current.dateComponents([.hour, .minute], from:  $0.finishDate).hour!)
-                )
-                .foregroundStyle($0.workTimeInSeconds == 0 ? .clear : .red)
-        }
-        .accessibilityIdentifier(Identifier.Chart.finishTimeChart.rawValue)
-        .chartYScale(domain: 0...24)
-    } // END OF VAR
-    
     var chartFinishTimeLegend: some View {
         HStack {
             Text("Legend:")
@@ -188,23 +224,8 @@ struct StatisticsView: View {
         .accessibilityIdentifier(Identifier.ChartLegend.finishTimeChartLegend.rawValue)
         .font(.caption)
     } // END OF VAR
-    
-    var chartTypePicker: some View {
-        Picker("Chart type", selection: $viewModel.chartType) {
-            Text("Time")
-                .tag(ChartType.time)
-                .accessibilityIdentifier(Identifier.ChartTypeButton.workTime.rawValue)
-            Text("Start time")
-                .tag(ChartType.startTime)
-                .accessibilityIdentifier(Identifier.ChartTypeButton.startTime.rawValue)
-            Text("Finish time")
-                .tag(ChartType.finishTime)
-                .accessibilityIdentifier(Identifier.ChartTypeButton.finishTime.rawValue)
-        }
-        .accessibilityIdentifier(Identifier.SegmentedControl.chartType.rawValue)
-        .pickerStyle(.segmented)
-    } // END OF VAR
 }
+
 
 //MARK: PREVIEW
 struct StatisticsView_Previews: PreviewProvider {
