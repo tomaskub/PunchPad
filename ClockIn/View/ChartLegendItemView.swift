4cd502433bb7f685642e1c6ee62bf60725302398
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-11-12 18:26:58 +0100

    Initial add of ChartLegendItem, ChartLegendItemView and ChartLegendView

---
+++ ClockIn/View/ChartLegendItemView.swift
@@ -0,0 +1,28 @@
+//
+//  ChartLegendItemView.swift
+//  ClockIn
+//
+//  Created by Tomasz Kubiak on 12/11/2023.
+//
+
+import SwiftUI
+
+struct ChartLegendItemView: View {
+    let chartLegendItem: ChartLegendItem
+    var body: some View {
+        HStack {
+            chartLegendItem.itemShape
+                .fill(chartLegendItem.itemShapeColor)
+                .frame(width: 14, height: 14)
+            Text(chartLegendItem.itemName)
+        }
+    }
+}
+
+struct ChartLegendItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartLegendItemView(chartLegendItem: ChartLegendItem(itemName: "Rectangles", itemShape: Rectangle(), itemShapeColor: .red))
+            .padding()
+            .previewLayout(.sizeThatFits)
+    }
+}
---
+++ ClockIn/View/ChartLegendView.swift
@@ -0,0 +1,35 @@
+//
+//  ChartLegendView.swift
+//  ClockIn
+//
+//  Created by Tomasz Kubiak on 10/11/2023.
+//
+
+import SwiftUI
+
+struct ChartLegendView: View {
+    let chartLegendItems: [ChartLegendItem]
+    var body: some View {
+        HStack {
+            Text("Legend:")
+            ForEach(chartLegendItems) { item in
+                ChartLegendItemView(chartLegendItem: item)
+            }
+        }
+    }
+}
+
+struct ChartLegendView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartLegendView(chartLegendItems: [
+            ChartLegendItem(itemName: "Rectangle", itemShape: Rectangle(), itemShapeColor: .blue),
+            ChartLegendItem(itemName: "Circle", itemShape: Circle(), itemShapeColor: .green)
+        ])
+        .padding()
+        .previewLayout(.sizeThatFits)
+        .previewDisplayName("Chart Legend View")
+    }
+}
+
+
+
