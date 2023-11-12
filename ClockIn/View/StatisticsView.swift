2ce2c55e5e31e9dd573070a30c7cba3a7e72acdb
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-11-12 19:21:58 +0100

    Initial add of ChartFactory Refactor StatisticsView to use chart factory

--- ClockIn/View/StatisticsView.swift
+++ ClockIn/View/StatisticsView.swift
@@ -50,21 +50,9 @@ struct StatisticsView: View {
                 
                 Section {
                     if viewModel.netPayAvaliable {
-                        ForEach(viewModel.salaryListDataNetPay, id: \.0) { data in
-                            HStack{
-                                Text(data.0)
-                                Spacer()
-                                Text(data.1)
-                            } // END OF HSTACK
-                        } // END OF FOR EACH
+                        netSalaryData
                     } // END OF IF
-                    ForEach(viewModel.salaryListDataGrossPay, id: \.0) { data in
-                        HStack{
-                            Text(data.0)
-                            Spacer()
-                            Text(data.1)
-                        } // END OF HSTACK
-                    } // END OF FOR EACH
+                     grossSalaryData
                 } header: {
                     sectionHeader(salaryCalculationHeaderText)
                         .accessibilityIdentifier(Identifier.SectionHeaders.salaryCalculation.rawValue)
@@ -123,108 +111,50 @@ extension StatisticsView {
     }
 }
 
+//MARK: CHART VIEW BUILDERS
 extension StatisticsView {
     @ViewBuilder
     var chart: some View {
         switch chartType {
         case .time:
-            chartWorkTime
-            chartWorkTimeLegend
+            ChartFactory.buildBarChart(entries: viewModel.entriesForChart, includeRuleMark: false)
         case .startTime:
-            chartStartTime
-            chartStartTimeLegend
+            ChartFactory.buildPointChartForPunchTime(
+                entries: viewModel.entriesForChart,
+                property: \.startDate,
+                color: .blue,
+                displayName: "Clock in")
         case .finishTime:
-            chartFinishTime
-            chartFinishTimeLegend
+            ChartFactory.buildPointChartForPunchTime(
+                entries: viewModel.entriesForChart, 
+                property: \.finishDate,
+                color: .red,
+                displayName: "Clock out")
         } // END OF SWITCH
     } // END OF VAR
-    
-    var chartWorkTime: some View {
-        Chart(viewModel.entriesForChart) {
-            RuleMark(y: .value("WorkGoal", 8))
-                .lineStyle(.init(dash: [10]))
-                .foregroundStyle(.red)
-            
-            BarMark(
-                x: .value("Date", $0.startDate, unit: .day),
-                y: .value("Hours worked", $0.workTimeInSeconds / 3600))
-            .foregroundStyle(.blue)
-            .cornerRadius(10)
-            BarMark(x: .value("Date", $0.startDate,unit: .day),
-                    y: .value("Hours worked", $0.overTimeInSeconds / 3600))
-            .foregroundStyle(.green)
-            .cornerRadius(10)
-        }
-        .accessibilityIdentifier(Identifier.Chart.workTimeChart.rawValue)
-        //Additional chart properties x-axis and y-scale
-        .chartYScale(domain: 0...15)
-        
-    } // END OF VAR
-    
-    var chartStartTime: some View {
-        Chart(viewModel.entriesForChart) {
-            PointMark(
-                x: .value("Date", $0.startDate),
-                y: .value("Date", Calendar.current.dateComponents([.hour, .minute], from:  $0.startDate).hour!)
-            )
-            .foregroundStyle($0.workTimeInSeconds == 0 ? .clear : .blue)
-        }
-        .accessibilityIdentifier(Identifier.Chart.startTimeChart.rawValue)
-        .chartYScale(domain: 0...24)
-    } // END OF VAR
-    var chartFinishTime: some View {
-        Chart(viewModel.entriesForChart) {
-            PointMark(
-                x: .value("Date", $0.startDate),
-                y: .value("Date", Calendar.current.dateComponents([.hour, .minute], from:  $0.finishDate).hour!)
-            )
-            .foregroundStyle($0.workTimeInSeconds == 0 ? .clear : .red)
-        }
-        .accessibilityIdentifier(Identifier.Chart.finishTimeChart.rawValue)
-        .chartYScale(domain: 0...24)
-    } // END OF VAR
 }
 
+//MARK: DATA VIEW BUILDERS
 extension StatisticsView {
-    var chartStartTimeLegend: some View {
-        HStack {
-            Text("Legend:")
-            Circle()
-                .fill(.blue)
-                .frame(width: 14, height: 14)
-            Text("Clock in")
+    var netSalaryData: some View {
+        ForEach(viewModel.salaryListDataNetPay, id: \.0) { data in
+            generateRow(label: data.0, value: data.1)
+        } // END OF FOR EACH
+    }
+    var grossSalaryData: some View {
+        ForEach(viewModel.salaryListDataGrossPay, id: \.0) { data in
+            generateRow(label: data.0, value: data.1)
         }
-        .accessibilityIdentifier(Identifier.ChartLegend.startTimeChartLegend.rawValue)
-        .font(.caption)
-    } // END OF VAR
-    var chartWorkTimeLegend: some View {
-        HStack {
-            Text("Legend:")
-            Rectangle()
-                .fill(.blue)
-                .frame(width: 14, height: 14)
-            Text("Working time")
-            Rectangle()
-                .fill(.green)
-                .frame(width: 14, height: 14)
-            Text("Overtime")
-        }
-        .accessibilityIdentifier(Identifier.ChartLegend.workTimeChartLegend.rawValue)
-        .font(.caption)
-    } // END OF VAR
-    var chartFinishTimeLegend: some View {
-        HStack {
-            Text("Legend:")
-            Circle()
-                .fill(.red)
-                .frame(width: 14, height: 14)
-            Text("Clock out")
-        }
-        .accessibilityIdentifier(Identifier.ChartLegend.finishTimeChartLegend.rawValue)
-        .font(.caption)
-    } // END OF VAR
+    }
+    @ViewBuilder
+    func generateRow(label: String, value: String) -> some View {
+        HStack{
+            Text(label)
+            Spacer()
+            Text(value)
+        } // END OF HSTACK
+    }
 }
-
 
 //MARK: PREVIEW
 struct StatisticsView_Previews: PreviewProvider {
