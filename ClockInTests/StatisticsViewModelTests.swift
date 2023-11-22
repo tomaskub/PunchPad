03b7a411c73e7114bd8b51529d4b88c9c34b5e86
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-11-22 23:55:39 +0100

    Add initial tests for StatisticsViewModel generatePeriod func

--- ClockInTests/StatisticsViewModelTests.swift
+++ ClockInTests/StatisticsViewModelTests.swift
@@ -42,4 +42,59 @@ final class StatisticsViewModelTests: XCTestCase {
         let result = sut.entriesForChart
         XCTAssert(result.count == correctValue, "There should be 7 objects in the array")
     }
+    
+    //TODO: FIX MISMATCH BETWEEN DATES FROM DATE FORMATTER AND CURRENT CALENDAR
+    func test_generatePeriod_forMonth() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        let inputDate = dateFormatter.date(from: "20/11/2023")
+        let startDate = dateFormatter.date(from: "01/11/2023")
+        let finishDate = dateFormatter.date(from: "30/11/2023")
+        
+        guard let inputDate, let startDate, let finishDate else {
+            XCTFail("Failed to generate input and predicted output dates")
+            return
+        }
+        
+        let result = sut.generatePeriod(for: inputDate, in: .month)
+        let expectedResult = (Calendar.current.startOfDay(for: startDate), Calendar.current.startOfDay(for: finishDate))
+        XCTAssertEqual(result.0, expectedResult.0, "Start date is \(result.0), and it should be \(expectedResult.0)")
+        XCTAssert(result.1 == expectedResult.1, "Finish date is \(result.1), and it should be \(expectedResult.1)")
+    }
+    
+    func test_generatePeriod_forWeek() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        let inputDate = dateFormatter.date(from: "22/11/2023")
+        let startDate = dateFormatter.date(from: "20/11/2023")
+        let finishDate = dateFormatter.date(from: "26/11/2023")
+        
+        guard let inputDate, let startDate, let finishDate else {
+            XCTFail("Failed to generate input and predicted output dates")
+            return
+        }
+        
+        let result = sut.generatePeriod(for: inputDate, in: .week)
+        let expectedResult = (Calendar.current.startOfDay(for: startDate), Calendar.current.startOfDay(for: finishDate))
+        XCTAssertEqual(result.0, expectedResult.0, "Start date is \(result.0), and it should be \(expectedResult.0)")
+        XCTAssert(result.1 == expectedResult.1, "Finish date is \(result.1), and it should be \(expectedResult.1)")
+    }
+    
+    func test_generatePeriod_forYear() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        let inputDate = dateFormatter.date(from: "20/11/2023")
+        let startDate = dateFormatter.date(from: "01/01/2023")
+        let finishDate = dateFormatter.date(from: "31/12/2023")
+        
+        guard let inputDate, let startDate, let finishDate else {
+            XCTFail("Failed to generate input and predicted output dates")
+            return
+        }
+        
+        let result = sut.generatePeriod(for: inputDate, in: .year)
+        let expectedResult = (Calendar.current.startOfDay(for: startDate), Calendar.current.startOfDay(for: finishDate))
+        XCTAssertEqual(result.0, expectedResult.0, "Start date is \(result.0), and it should be \(expectedResult.0)")
+        XCTAssert(result.1 == expectedResult.1, "Finish date is \(result.1), and it should be \(expectedResult.1)")
+    }
 }
