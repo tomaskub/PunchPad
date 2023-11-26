08300248b0adcf446c556cabd2251af790553b51
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-11-26 18:35:00 +0100

    Add tests for advancing period function

--- ClockInTests/ChartPeriodServiceTests.swift
+++ ClockInTests/ChartPeriodServiceTests.swift
@@ -201,3 +201,80 @@ extension ChartPeriodServiceTests {
         }
     }
 }
+
+//MARK: TESTS FOR ADVANCING PERIOD
+extension ChartPeriodServiceTests {
+    func test_advancingPeriodForWeek() throws {
+        // Given
+        guard let inputStartDate = Calendar.current.date(from: DateComponents(year: 2023, month: 11, day: 13)),
+              let inputFinishDate = Calendar.current.date(from: DateComponents(year: 2023, month: 11, day: 19)),
+              let expectedStartDate = Calendar.current.date(from: DateComponents(year: 2023, month: 11, day: 20)),
+              let expectedFinishDate = Calendar.current.date(from: DateComponents(year: 2023, month: 11, day: 26)) else {
+                XCTFail("Failed to generate input and predicted output dates")
+                return
+            }
+        let inputPeriod: Period = (inputStartDate, inputFinishDate)
+        let expectedResult: Period = (expectedStartDate, expectedFinishDate)
+        
+        // When
+        let result = try sut.advancePeriod(by: .week, from: inputPeriod)
+        
+        // Then
+        XCTAssertEqual(result.0, expectedResult.0, "Start date is \(result.0), and it should be \(expectedResult.0)")
+        XCTAssertEqual(result.1, expectedResult.1, "Finish date is \(result.1), and it should be \(expectedResult.1)")
+    }
+    
+    func test_advancingPeriodForMonth() throws {
+        // Given
+        guard let inputStartDate = Calendar.current.date(from: DateComponents(year: 2023, month: 10, day: 1)),
+              let inputFinishDate = Calendar.current.date(from: DateComponents(year: 2023, month: 10, day: 31)),
+              let expectedStartDate = Calendar.current.date(from: DateComponents(year: 2023, month: 11, day: 1)),
+              let expectedFinishDate = Calendar.current.date(from: DateComponents(year: 2023, month: 11, day: 30)) else {
+                XCTFail("Failed to generate input and predicted output dates")
+                return
+            }
+        let inputPeriod: Period = (inputStartDate, inputFinishDate)
+        let expectedResult: Period = (expectedStartDate, expectedFinishDate)
+        
+        // When
+        let result = try sut.advancePeriod(by: .month, from: inputPeriod)
+        
+        // Then
+        XCTAssertEqual(result.0, expectedResult.0, "Start date is \(result.0), and it should be \(expectedResult.0)")
+        XCTAssertEqual(result.1, expectedResult.1, "Finish date is \(result.1), and it should be \(expectedResult.1)")
+    }
+    
+    func test_advancingPeriodForYear() throws {
+        // Given
+        guard let inputStartDate = Calendar.current.date(from: DateComponents(year: 2022, month: 1, day: 1)),
+              let inputFinishDate = Calendar.current.date(from: DateComponents(year: 2022, month: 12, day: 31)),
+              let expectedStartDate = Calendar.current.date(from: DateComponents(year: 2023, month: 1, day: 1)),
+              let expectedFinishDate = Calendar.current.date(from: DateComponents(year: 2023, month: 12, day: 31)) else {
+                XCTFail("Failed to generate input and predicted output dates")
+                return
+            }
+        let inputPeriod: Period = (inputStartDate, inputFinishDate)
+        let expectedResult: Period = (expectedStartDate, expectedFinishDate)
+        
+        // When
+        let result = try sut.advancePeriod(by: .year, from: inputPeriod)
+        
+        // Then
+        XCTAssertEqual(result.0, expectedResult.0, "Start date is \(result.0), and it should be \(expectedResult.0)")
+        XCTAssertEqual(result.1, expectedResult.1, "Finish date is \(result.1), and it should be \(expectedResult.1)")
+    }
+    
+    func test_advancingPeriodForAll_throwsError() throws {
+        // Given
+        guard let inputStartDate = Calendar.current.date(from: DateComponents(year: 2023, month: 1, day: 1)),
+              let inputFinishDate = Calendar.current.date(from: DateComponents(year: 2023, month: 12, day: 31)) else {
+            XCTFail("Failed to generate input and predicted output dates")
+            return
+        }
+        let inputPeriod: Period = (inputStartDate, inputFinishDate)
+        
+        XCTAssertThrowsError(try sut.advancePeriod(by: .all, from: inputPeriod), "Function should throw") { error in
+            XCTAssertEqual(error as? ChartPeriodServiceError, .attemptedToRetrievePeriodForAll, "Error thrown should be `.attemptedToRetrievePeriodForAll`")
+        }
+    }
+}
