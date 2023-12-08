07bc0d11f4ffc8d7801a457fbb336e207a18903a
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-12-08 22:05:13 +0100

    Add groupEntries var Add func groupentriesByYearMonth

--- ClockIn/ViewModel/HistoryViewModel.swift
+++ ClockIn/ViewModel/HistoryViewModel.swift
@@ -10,7 +10,6 @@ import CoreData
 import Combine
 
 class HistoryViewModel: ObservableObject {
-    
     @Published private var dataManager: DataManager
     @Published var paginationState: PaginationState = .idle
     @Published var isMoreEntriesAvaliable: Bool
@@ -32,13 +31,41 @@ class HistoryViewModel: ObservableObject {
            return true
         }()
         self.entries = dataManager.entryThisMonth
-        
+        self.groupedEntries = groupEntriesByYearMonth(dataManager.entryArray)
         dataManager.objectWillChange.sink(receiveValue: { [weak self] _ in
             self?.objectWillChange.send()
         }).store(in: &subscriptions)
     }
     
     var entries: [Entry]
+    
+    var groupedEntries: [[Entry]] = []
+        
+    
+    func groupEntriesByYearMonth(_ entries: [Entry]) -> [[Entry]] {
+        var result: [[Entry]] = .init()
+        
+        var currentYearMonth: DateComponents?
+        var currentEntries: [Entry] = .init()
+        
+        for entry in dataManager.entryArray {
+            let entryDateComponents = Calendar.current.dateComponents([.month,.year], from: entry.startDate)
+            if entryDateComponents != currentYearMonth {
+                if !currentEntries.isEmpty {
+                    result.append(currentEntries)
+                }
+                currentEntries = [entry]
+                currentYearMonth = entryDateComponents
+            } else {
+                currentEntries.append(entry)
+            }
+        }
+        if !currentEntries.isEmpty {
+            result.append(currentEntries)
+        }
+            
+        return result
+    }
     
     /// provide a formatted string describing the amount of hours between start and finish date in an Entry object
     func timeWorkedLabel(for entry: Entry) -> String {
