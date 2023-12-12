c53e5856d36d8d42c1627e3bf15b1b5ca4a0aad1
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-12-12 23:28:55 +0100

    Initial add of tests for applying and reseting filters

--- ClockInTests/HistoryViewModelTests.swift
+++ ClockInTests/HistoryViewModelTests.swift
@@ -25,6 +25,7 @@ final class HistoryViewModelTests: XCTestCase {
     override func tearDown() {
         super.tearDown()
         sut = nil
+        container = nil
     }
     
     func test_groupingEntries() {
@@ -88,6 +89,59 @@ final class HistoryViewModelTests: XCTestCase {
         XCTAssertEqual(sut.groupedEntries.flatMap({ $0 }).count, chunkSize * 2, "Number of entries should be equal to two times chunk size")
     }
     
+    func test_applyFilters() {
+        sut = nil
+        let calendar = Calendar.current
+        guard let date = calendar.date(from: DateComponents(year: 2023, month: 12)) else { XCTFail(); return }
+        
+        let entries = PreviewDataFactory.buildDataForPreviewForYear(containing: date, using: calendar)
+        
+        for entry in entries {
+            container.dataManager.updateAndSave(entry: entry)
+        }
+        let chunkSize = 30
+        sut = .init(dataManager: container.dataManager,
+                    settingsStore: container.settingsStore,
+                    sizeOfChunk: chunkSize)
+        guard let fromDate =  calendar.date(from: DateComponents(year: 2023, month: 11, day: 6)),
+              let toDate = calendar.date(from: DateComponents(year: 2023, month: 11, day: 11)) else {
+            XCTFail()
+            return
+        }
+        sut.filterFromDate = fromDate
+        sut.filterToDate = toDate
+        sut.applyFilters()
+        XCTAssertEqual(sut.isSortingActive, true, "Sorting shoud be active")
+        XCTAssertEqual(sut.groupedEntries.flatMap({ $0 }).count, 5, "There should be 5 entries")
+        
+    }
+    func test_resetFilters() {
+        sut = nil
+        let calendar = Calendar.current
+        guard let date = calendar.date(from: DateComponents(year: 2023, month: 12)) else { XCTFail(); return }
+        
+        let entries = PreviewDataFactory.buildDataForPreviewForYear(containing: date, using: calendar)
+        
+        for entry in entries {
+            container.dataManager.updateAndSave(entry: entry)
+        }
+        let chunkSize = 30
+        sut = .init(dataManager: container.dataManager,
+                    settingsStore: container.settingsStore,
+                    sizeOfChunk: chunkSize)
+        guard let fromDate =  calendar.date(from: DateComponents(year: 2023, month: 12, day: 4)),
+              let toDate = calendar.date(from: DateComponents(year: 2023, month: 12, day: 8)) else {
+            XCTFail()
+            return
+        }
+        sut.filterFromDate = fromDate
+        sut.filterToDate = toDate
+        sut.applyFilters()
+        
+        sut.resetFilters()
+        XCTAssertEqual(sut.groupedEntries.flatMap({ $0 }).count, chunkSize, "Number of entries should be equal to two times chunk size")
+        
+    }
     func testConvertOvertimeToFraction_for2hours30minutes() {
         let startDate = Calendar.current.date(byAdding: .hour, value: -7 , to: Date())!
         let finishDate = Calendar.current.date(byAdding: .hour, value: 2 , to: Date())!
