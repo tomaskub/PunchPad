b5ad43c05e38585328f4d869bb36f9702a40d78e
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-12-25 21:03:08 +0100

    Add second timer for overtime and resumeTimerService func

--- ClockIn/ViewModel/HomeViewModel.swift
+++ ClockIn/ViewModel/HomeViewModel.swift
@@ -7,10 +7,11 @@
 
 import SwiftUI
 import Combine
+//TODO: Resume from background does not work on second timer - move to VM
+//TODO: fix bug when time in background is bigger than timer limit - first timer never emits finish - move to VM
+//TODO: Remove bug when timer is not saving entry when not stopped with button - move to VM
 
 class HomeViewModel: NSObject, ObservableObject {
-    
-    //MARK: DATA MANAGING PROPERTIES
     private var dataManager: DataManager
     private var settingsStore: SettingsStore
     private var payManager: PayManager
@@ -18,16 +19,29 @@ class HomeViewModel: NSObject, ObservableObject {
     private var finishDate: Date?
     private var appDidEnterBackgroundDate: Date?
     private var subscriptions = Set<AnyCancellable>()
-    //TIMER
     private var timerProvider: Timer.Type = Timer.self
     private var workTimerService: TimerService
-    
+    private var overtimeTimerService: TimerService?
     @Published var state: TimerService.TimerServiceState = .notStarted
+    
     var timerDisplayValue: TimeInterval {
-        return workTimerService.counter
+        if let overtimeTimerService = overtimeTimerService {
+            if overtimeTimerService.progress > 0 {
+                return overtimeTimerService.counter
+            } else {
+                return workTimerService.counter
+            }
+        } else {
+            return workTimerService.counter
+        }
     }
+    
     var normalProgress: CGFloat {
         return workTimerService.progress
+    }
+    
+    var overtimeProgress: CGFloat {
+        return overtimeTimerService?.progress ?? 0
     }
     
     init(dataManager: DataManager, settingsStore: SettingsStore, payManager: PayManager ,timerProvider: Timer.Type) {
@@ -40,26 +54,51 @@ class HomeViewModel: NSObject, ObservableObject {
             timerProvider: timerProvider,
             timerLimit: TimeInterval(settingsStore.workTimeInSeconds)
         )
+        if self.settingsStore.isLoggingOvertime {
+            self.overtimeTimerService = .init(
+                timerProvider: timerProvider,
+                timerLimit: TimeInterval(settingsStore.maximumOvertimeAllowedInSeconds))
+        }
         super.init()
         setAppStateObservers()
         setUpTimerSubscribers()
     }
+    
     private func setUpTimerSubscribers() {
         self.workTimerService.objectWillChange.sink { _ in
             self.objectWillChange.send()
         }.store(in: &subscriptions)
         
-        // this is a subscriber for saving when timer is set to finished
-        self.workTimerService.$serviceState.filter { state in
-            return state == .finished
-        }.sink { [weak self] _ in
-            self?.saveEntry()
+        self.overtimeTimerService?.objectWillChange.sink { _ in
+            self.objectWillChange.send()
         }.store(in: &subscriptions)
+        
+        if let overtimeTimerService = self.overtimeTimerService {
+            self.workTimerService.$serviceState.filter { state in
+                state == .finished
+            }.sink { [weak self] _ in
+                self?.overtimeTimerService?.send(event: .start)
+            }.store(in: &subscriptions)
+            
+            overtimeTimerService.$serviceState.filter { state in
+                return state == .finished
+            }.sink { [weak self] _ in
+                self?.saveEntry()
+            }.store(in: &subscriptions)
+        } else {
+            self.workTimerService.$serviceState.filter { state in
+                return state == .finished
+            }.sink { [weak self] _ in
+                
+                self?.saveEntry()
+            }.store(in: &subscriptions)
+        }
     }
 }
 
 //MARK: TIMER INTERFACE
 extension HomeViewModel {
+    // should start a new timer
     func startTimerService() {
         if workTimerService.serviceState == .finished {
             workTimerService = .init(
@@ -78,14 +117,19 @@ extension HomeViewModel {
     func pauseTimerService() {
         self.state = .paused
         workTimerService.send(event: .pause)
+        overtimeTimerService?.send(event: .pause)
     }
-    
+    func resumeTimerService() {
+        self.state = .running
+        workTimerService.send(event: .resumeWith(nil))
+        overtimeTimerService?.send(event: .resumeWith(nil))
+    }
     func stopTimerService() {
         self.state = .notStarted
-        workTimerService.send(event: .stop)
         finishDate = Date()
+        workTimerService.send(event: .stop)
+        overtimeTimerService?.send(event: .stop)
     }
-    
     func resumeFromBackground(_ appDidEnterBackgroundDate: Date) {
         let timePassedInBackground = DateInterval(start: appDidEnterBackgroundDate, end: Date()).duration
         workTimerService.send(event: .resumeWith(timePassedInBackground))
