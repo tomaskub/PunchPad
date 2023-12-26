e0e130cb0c4628278ead8f3ffc2efcc7aee96a9c
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-12-26 23:29:00 +0100

    Initial add of circle button style

---
+++ ClockIn/View/ViewComponent/CircleButton.swift
@@ -0,0 +1,55 @@
+//
+//  CircularButton.swift
+//  ClockIn
+//
+//  Created by Tomasz Kubiak on 26/12/2023.
+//
+
+import SwiftUI
+
+struct CircleButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .foregroundColor(.theme.white)
+            .padding()
+            .background(
+                    Circle()
+                        .foregroundColor(.theme.buttonColor)
+            )
+            .overlay {
+                Circle()
+                    .stroke(lineWidth: 8)
+                    .padding(4)
+                    .foregroundColor(.theme.white)
+            }
+            .shadow(color: .black.opacity(0.1), radius: 20, x: 0, y: 4)
+    }
+    
+}
+
+#Preview {
+    ZStack{
+        BackgroundFactory.buildSolidColor()
+        HStack {
+            Button {
+                //nothing
+            } label: {
+                Image(systemName: "play.fill")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .offset(x: 5, y: 0)
+                    .frame(width: 100, height: 100)
+            }
+            .buttonStyle(CircleButton())
+            Button {
+                // do nothing
+            } label: {
+                Image(systemName: "pause.fill")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .frame(width: 60, height: 60)
+            }
+            .buttonStyle(CircleButton())
+        }
+    }
+}
