96e9a51115a25d33c32ea219eac799c505817016
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-12-26 20:49:58 +0100

    Add color theme from figma

--- ClockIn/Extensions/ColorTheme.swift
+++ ClockIn/Extensions/ColorTheme.swift
@@ -12,9 +12,32 @@ extension Color {
 }
 
 struct ColorTheme {
-    let accent = Color("GreenColorScheme/AccentColor")
+    /// Green background color
     let background = Color("GreenColorScheme/BackgroundColor")
-    let darkGreen = Color("GreenColorScheme/DarkGreen")
-    let lightGreen = Color("GreenColorScheme/LightGreen")
-    let mutedGreen = Color("GreenColorScheme/MutedGreen")
+    /// Semi-black color used for text in headings
+    let black = Color("GreenColorScheme/Black")
+    /// Semi-black color used for text in labels
+    let blackLabel = Color("GreenColorScheme/BlackText")
+    /// Timer button fill green color
+    let buttonColor = Color("GreenColorScheme/ButtonColor")
+    /// Green color used for filling ring
+    let ringGreen = Color("GreenColorScheme/CircleRingGreen")
+    /// White-gray color used for unfilled ring
+    let ringWhite = Color("GreenColorScheme/CircleRingUnfilled")
+    /// Primary color used for buttons, labels and icons
+    let primary = Color("GreenColorScheme/Primary")
+    /// Red color for chart  bars
+    let redChart = Color("GreenColorScheme/RedChart")
+    /// Start color for ring red gradient
+    let redGradientStart = Color("GreenColorScheme/RedGradientStart")
+    /// End color for ring red gradient
+    let redGradientEnd = Color("GreenColorScheme/RedGradientEnd")
+    /// Red color used for timer label in overtime
+    let redLabel = Color("GreenColorScheme/RedLabel")
+    /// Gray color used for timer label in worktime
+    let secondaryLabel = Color("GreenColorScheme/SecondaryWorktimeLabel")
+    /// Gray opaque color used for background in segmented pickers
+    let tertiary = Color("GreenColorScheme/Tertiary")
+    /// White color used for background elements of tab bars, list rows and navigation bars
+    let white = Color("GreenColorScheme/White")
 }
