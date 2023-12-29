e12567162da16e5408a5ef129be18ff1abbb99ec
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-12-29 16:03:31 +0100

    Initial add of custom navigation bar

---
+++ NavigationKit/NavigationKit/CustomNavigationBar.swift
@@ -0,0 +1,43 @@
+//
+//  CustomNavigationBar.swift
+//  NavigationKit
+//
+//  Created by Tomasz Kubiak on 29/12/2023.
+//
+
+import SwiftUI
+
+struct CustomNavigationBar: View {
+    let config: NavBarTitleConfiguration
+    let shouldShowBackButton: Bool
+    let background: EquatableView
+    let leadingElements: EquatableView
+    let trailingElements: EquatableView
+    let shouldElevate: Bool
+    let onBackTapped: () -> Void
+    
+    var body: some View {
+        //Custom implementation of the appearance of the bar
+        HStack {
+            leadingElements.view
+            Spacer()
+            Text(config.title)
+                .foregroundColor(config.textColor)
+            Spacer()
+            trailingElements.view
+        }
+    }
+}
+
+#Preview {
+    CustomNavigationBar(config: NavBarTitleConfiguration(title: "Preview",
+                                                         textColor: .black,
+                                                         alignment: .center),
+                        shouldShowBackButton: true,
+                        background: .init(view: AnyView(Color.blue)),
+                        leadingElements: .init(view: AnyView(Image(systemName: "plus"))),
+                        trailingElements: .init(view: AnyView(Image(systemName:"heart.fill"))),
+                        shouldElevate: false) {
+        print("back tapped")
+    }
+}
