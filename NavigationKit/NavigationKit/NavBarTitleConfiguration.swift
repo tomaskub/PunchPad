897ce791db2302e880775e8482bce5fbdebdba5e
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-12-29 15:48:03 +0100

    Initial add of NavBarTitleConfiguration

---
+++ NavigationKit/NavigationKit/NavBarTitleConfiguration.swift
@@ -0,0 +1,14 @@
+//
+//  NavBarTitleConfiguration.swift
+//  NavigationKit
+//
+//  Created by Tomasz Kubiak on 29/12/2023.
+//
+
+import SwiftUI
+
+public struct NavBarTitleConfiguration: Equatable {
+    public var title: String
+    public var textColor: Color
+    public var alignment: Alignment
+}
