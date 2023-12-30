26e675930f791bb0f3abcbd6b2888a3b3ae8f49b
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-12-30 02:22:56 +0100

    Add font property to NavBarTitleConfiguration

--- NavigationKit/NavigationKit/Model/NavBarTitleConfiguration.swift
+++ NavigationKit/NavigationKit/Model/NavBarTitleConfiguration.swift
@@ -10,11 +10,14 @@ import SwiftUI
 public struct NavBarTitleConfiguration: Equatable {
     public var title: String
     public var textColor: Color
+    public var font: Font
     public var alignment: Alignment
     
-    public init(title: String, textColor: Color = .primary, alignment: Alignment = .leading) {
+    
+    public init(title: String, textColor: Color = .primary, font: Font = .title, alignment: Alignment = .leading) {
         self.title = title
         self.textColor = textColor
         self.alignment = alignment
+        self.font = font
     }
 }
