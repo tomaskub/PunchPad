c7fe92954e0c9eef177095752564a443710919d0
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-12-30 17:53:29 +0100

    Initial add of theme kit

---
+++ ThemeKit/ThemeKit/ThemeKit.h
@@ -0,0 +1,18 @@
+//
+//  ThemeKit.h
+//  ThemeKit
+//
+//  Created by Tomasz Kubiak on 30/12/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ThemeKit.
+FOUNDATION_EXPORT double ThemeKitVersionNumber;
+
+//! Project version string for ThemeKit.
+FOUNDATION_EXPORT const unsigned char ThemeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThemeKit/PublicHeader.h>
+
+
