fd0735eabbbf4b8ed230cf41ee17ec93f084230b
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2024-01-03 19:36:32 +0100

    Add GreenBorderTextFieldStyle Add extension to TextFieldStyle Move styles to seperate group

---
+++ ClockIn/Extensions/TextFieldStyle+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  TextFieldStyle.swift
+//  ClockIn
+//
+//  Created by Tomasz Kubiak on 03/01/2024.
+//
+
+import SwiftUI
+
+extension TextFieldStyle where Self == GreenBorderTextFieldStyle {
+    static var greenBordered: GreenBorderTextFieldStyle { .init() }
+}
