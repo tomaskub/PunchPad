865cf70c101ca5417cefef7acaf0c8ee39d27b66
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2024-01-07 13:20:55 +0100

    Add test for period placed in the past

--- ClockInTests/PayManagerTests.swift
+++ ClockInTests/PayManagerTests.swift
@@ -10,67 +10,111 @@ import CoreData
 @testable import ClockIn
 
 final class PayManagerTests: XCTestCase {
-
     var sut: PayManager!
-    var container: Container!
+    var settingsStore: SettingsStore!
+    var dataManager: DataManager!
+    var periodService: ChartPeriodService!
+    
     override func setUp() {
         super.setUp()
-        container = Container()
-        sut = .init(dataManager: container.dataManager,
-                    settingsStore: container.settingsStore)
-    }
-
-    override func tearDown() {
-        super.tearDown()
-        DataManager.testing.deleteAll()
-        sut = nil
+        dataManager = DataManager.testing
+        dataManager.deleteAll()
+        SettingsStore.setTestUserDefaults()
+        settingsStore = SettingsStore()
+        sut = .init(dataManager: dataManager,
+                    settingsStore: settingsStore)
+        periodService = ChartPeriodService(calendar: .current)
     }
     
-    func testGetNumberOfWorkingDays() {
-        
-        let formatter = DateFormatter()
-        formatter.dateFormat = "MMMM d, yyyy"
-        let dateString = "April 16, 2023"
-        
-        
-        guard let date = formatter.date(from: dateString) else {
+    override func tearDown() {
+        super.tearDown()
+        sut = nil
+        settingsStore = nil
+        dataManager = nil
+    }
+    
+    func testGeneratingDataForPastPeriod() {
+        //Given
+        guard let date = generateStableDate(),
+        let testPeriod = try? periodService.generatePeriod(for: date, in: .month) else {
             XCTFail("Failed to build date from given string")
             return
         }
-        
-        let numberOfDays = sut.getNumberOfWorkingDays(inMonthOfDate: date)
-        
-        XCTAssert(numberOfDays == 20, "Number of working days should be 20" )
+        let numberOfWorkingDays = addTestData(forPeriod: testPeriod,
+                                              workTimeInSec: 8 * 3600,
+                                              overtimeInSec: 0,
+                                              standardWorkTimeInSec: 8 * 3600,
+                                              maximumOvertimeInSec: 5 * 3600,
+                                              grossPayPerMonth: settingsStore.grossPayPerMonth
+        )
+        //When
+        sut.updatePeriod(with: testPeriod)
+        //Then
+        let result = sut.grossDataForPeriod
+        let expectedPayHour = Double(settingsStore.grossPayPerMonth) / Double(numberOfWorkingDays * 8)
+        XCTAssertEqual(result.numberOfWorkingDays, numberOfWorkingDays)
+        XCTAssertEqual(result.payPerHour, expectedPayHour)
+        XCTAssertNil(result.payPredicted)
+        XCTAssertEqual(result.payUpToDate, Double(settingsStore.grossPayPerMonth))
     }
-    
-    func testCalculateGrossPay_whenNoDateIsGiven() {
-        //Add entries to memory
-        let components = Calendar.current.dateComponents([.month, .year], from: Date())
-        
-        guard let startOfTheMonth = Calendar.current.date(from: components) else {
-            XCTFail()
-            return
+}
+
+
+extension PayManagerTests {
+    /// Returns a date object representing start of day of April 16, 2023
+    func generateStableDate() -> Date? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMMM d, yyyy"
+        let dateString = "April 16, 2023"
+        return formatter.date(from: dateString)
+    }
+    /// Add test data for a given period, using entries constructed from input parameters. Entries are saved in testing data manager instance.
+    /// - Parameters:
+    ///     - period: period in which to create test data
+    ///     - workTimeInSec: time worked in entry
+    ///     - overtimeInSec: overtime in entry
+    ///     - standardWorkTimeInSec: standard work time for entry
+    ///     - maximumOvertimeInSec: maximum amount of overtime for entry
+    ///     - grossPayPerMont: gross pay per month in entry
+    /// - Returns: number of working days in period
+    func addTestData(forPeriod period: Period,
+                     workTimeInSec: Int,
+                     overtimeInSec: Int,
+                     standardWorkTimeInSec: Int,
+                     maximumOvertimeInSec: Int,
+                     grossPayPerMonth: Int) -> Int {
+        let failMessage = "Add test data failed"
+        guard let numberOfDays = Calendar.current.dateComponents([.day], from: period.0, to: period.1).day else {
+            XCTFail(failMessage)
+            return 0
         }
-        
-        for i in 1...5 {
-            let date = Calendar.current.startOfDay(for: Calendar.current.date(byAdding: .day, value: i, to: startOfTheMonth)!)
-            let entry = Entry(
-                startDate:  Calendar.current.date(byAdding: .hour, value: 8, to: date)!,
-                finishDate: Calendar.current.date(byAdding: .hour, value: 16, to: date)!,
-                workTimeInSec: 8 * 3600,
-                overTimeInSec: 0,
-                maximumOvertimeAllowedInSeconds: 5*3600,
-                standardWorktimeInSeconds: 8*3600,
-                grossPayPerMonth: container.settingsStore.grossPayPerMonth,
-                calculatedNetPay: nil)
+        var dateArray = [Date]()
+        for i in 0..<numberOfDays {
+            guard let date = Calendar.current.date(byAdding: .day, value: i, to: period.0) else {
+                XCTFail(failMessage)
+                return 0
+            }
+            dateArray.append(date)
+        }
+        var filteredArray = dateArray.filter { date in
+            !Calendar.current.isDateInWeekend(date)
+        }
+        let resultArray = filteredArray.map { date in
+            let startOfDate = Calendar.current.startOfDay(for: date)
+            return Entry(
+                startDate:  Calendar.current.date(byAdding: .hour, value: 8, to: startOfDate)!,
+                finishDate: Calendar.current.date(byAdding: .hour, value: 16, to: startOfDate)!,
+                workTimeInSec: workTimeInSec,
+                overTimeInSec: overtimeInSec,
+                maximumOvertimeAllowedInSeconds: maximumOvertimeInSec,
+                standardWorktimeInSeconds: standardWorkTimeInSec,
+                grossPayPerMonth: grossPayPerMonth,
+                calculatedNetPay: nil
+            )
+        }
+        for entry in resultArray {
             DataManager.testing.updateAndSave(entry: entry)
         }
-        
-        let pay = sut.grossPayPerHour
-        let numberOfHours: Double = 40
-        let correctResult = numberOfHours * pay
-        
-        let result = sut.calculateGrossPay()
-        XCTAssert(result == correctResult, "Result from function should be equal to \(correctResult)")
+        return resultArray.count
     }
 }
