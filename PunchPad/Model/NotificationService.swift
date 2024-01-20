37ce1587c7effa40f256116a4f79e138c8723095
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2024-01-20 16:40:57 +0100

    Add initial implementation of the service

--- PunchPad/Model/NotificationService.swift
+++ PunchPad/Model/NotificationService.swift
@@ -6,7 +6,60 @@
 //
 
 import Foundation
+import UserNotifications
 
 class NotificationService {
+    let center: UNUserNotificationCenter
+    var pendingNotificationsIDs: Set<String> = []
+    
+    init(center: UNUserNotificationCenter) {
+        self.center = center
+    }
+    
+    func requestAuthorizationForNotifications(failureHandler: @escaping (Bool, Error?) -> Void) {
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { success, error in
+            if !success {
+                // setting in store should be false
+                failureHandler(success, error) // <- this happens when user does not accept the notifications
+            }
+            if let error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    func deschedulePendingNotifications() {
+        center.removeAllPendingNotificationRequests()
+    }
+    
+    func scheduleNotification(for notification: AppNotification, in timeInterval: TimeInterval) {
+        let id = UUID().uuidString
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: false)
+        
+        let notificationContent = UNMutableNotificationContent()
+        notificationContent.title = notification.title
+        notificationContent.body = notification.body
+        
+        let request = UNNotificationRequest(identifier: id,
+                                            content: notificationContent,
+                                            trigger: trigger)
+        checkForPermissionAndDispatch(request)
+    }
+    
+    func checkForPermissionAndDispatch(_ notificationRequest: UNNotificationRequest) {
+        center.getNotificationSettings { settings in
+            switch settings.authorizationStatus {
+            case .authorized:
+                self.dispatch(notification: notificationRequest)
+            default:
+                return
+            }
+        }
+    }
+    
+    func dispatch(notification: UNNotificationRequest) {
+        _ = pendingNotificationsIDs.insert(notification.identifier)
+        center.add(notification)
+    }
     
 }
