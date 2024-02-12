a288e3a314bf46aa37e4bbca91ed78e79de09824
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2024-02-12 23:33:17 +0100

    Initial add of XCTestCase extensions Add awaitPublisher func

---
+++ PunchPadTests/Extensions/XCTestCase+Extension.swift
@@ -0,0 +1,36 @@
+//
+//  XCTestCase+Extension.swift
+//  PunchPadTests
+//
+//  Created by Tomasz Kubiak on 12/02/2024.
+//
+
+import XCTest
+import Combine
+
+extension XCTestCase {
+    func awaitPublisher<T: Publisher>(_ publisher: T, timeout: TimeInterval = 2.5, file: StaticString = #file, line: UInt = #line) throws -> T.Output {
+        var result: Result<T.Output, Error>?
+        let expectation = self.expectation(description: "Awaiting publisher")
+        
+        let cancellable = publisher.sink { completion in
+            switch completion {
+            case .failure(let error):
+                result = .failure(error)
+            case .finished:
+                break
+            }
+            expectation.fulfill()
+        } receiveValue: { value in
+            result = .success(value)
+        }
+        waitForExpectations(timeout: timeout)
+        cancellable.cancel()
+        let unwrappedResult = try XCTUnwrap(result,
+                                            "Awaited publisher did not produce any output",
+                                            file: file,
+                                            line: line)
+        return try unwrappedResult.get()
+    }
+}
+
