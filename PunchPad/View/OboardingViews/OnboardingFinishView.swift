6e661c6ab0c9b1c43963270dcfe98c3aeb8892d5
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2024-04-09 21:41:36 +0200

    Remove commented code chunk

--- PunchPad/View/OboardingViews/OnboardingFinishView.swift
+++ PunchPad/View/OboardingViews/OnboardingFinishView.swift
@@ -15,31 +15,6 @@ struct OnboardingFinishView: View {
         VStack(spacing: 40) {
             title
             description
-            //This section is still in works until in-app tutorial is developed
-            /*
-             Text("To enter a short tutorial on how to use the app and its functions click the tour button. Alternatively, if you are a person that assembles the IKEA furniture without looking at the instructions click the finish button")
-             .multilineTextAlignment(.center)
-             
-             
-             Text("Finish!")
-             .font(.headline)
-             .foregroundColor(.accentColor)
-             .frame(height: 55)
-             .frame(maxWidth: .infinity)
-             .background(Color.primary.colorInvert())
-             .cornerRadius(10)
-             .overlay {
-             HStack {
-             Spacer()
-             Image(systemName: "bicycle")
-             Image(systemName: "figure.walk")
-             }
-             .padding()
-             }
-             .onTapGesture {
-             dismiss()
-             }
-             */
         }
         .padding(30)
     }
