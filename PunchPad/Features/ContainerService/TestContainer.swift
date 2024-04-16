31c34eca72977207403b2e3278dd212f2630e201
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2024-04-16 10:52:25 +0200

    Seperate implementations in Container to seperate classes - Test and Preview

--- PunchPad/Features/ContainerService/TestContainer.swift
+++ PunchPad/Features/ContainerService/TestContainer.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+final class TestContainer: ContainerProtocol {
+    private(set) var dataManager: DataManager
+    private(set) var payManager: PayManager
+    private(set) var timerProvider: Timer.Type
+    private(set) var settingsStore: SettingsStore
+    private(set) var notificationService: NotificationService
+    
+    init() {
+        self.timerProvider = Timer.self
+        SettingsStore.setTestUserDefaults()
+        self.settingsStore = SettingsStore()
+        self.dataManager = .testing
+        self.payManager = PayManager(dataManager: .testing, settingsStore: settingsStore, calendar: .current)
+        self.notificationService = NotificationService(center: .current())
+    }
+}
