4c7ed7aed20d8b6cdcf9aa3693c94a0566b9a8ec
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2025-03-19 21:28:24 +0100

    Fix script typo

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 set -e 
-set -0 pipefail
+set -o pipefail
+
 print_step() {
   echo ""
   echo "================================================"
@@ -9,13 +10,20 @@ print_step() {
   echo "================================================"
 }
 
+install_dependecy() {
+  echo "Installing $1 with brew..."
+  HOMEBREW_NO_AUTO_UPDATE=1 HOMEBREW_NO_INSTALL_CLEANUP=1 HOMEBREW_NO_ENV_HINTS=1 brew install --quiet $1
+  echo "$1 installed successfully"
+}
+
 print_step "Starting $CI_XCODE_PROJECT"
 
 print_step "Checking xcodegen availablity"
 
 if ! command -v xcodegen &> /dev/null; then 
-  echo "XcodeGen not found, installing with brew..."
-  brew install xcodegen
+  echo "XcodeGen not found"
+  install_dependecy "xcodegen"
+  #brew install xcodegen
 else 
   echo "XcodeGen avaliable"
 fi
@@ -34,8 +42,8 @@ echo "Project generated successfully"
 
 print_step "Resolving swift package dependencies"
 xcodebuild -resolvePackageDependencies \
-  -project "PunchPad.xcodeproj" \ 
-  -scheme "PunchPad" \ 
+  -project "PunchPad.xcodeproj" \
+  -scheme "PunchPad" \
   -derivedDataPath "$CI_DERIVED_DATA_PATH" \
   -clonedSourcePackagesDirPath "./SourcePackages" \
   -disableAutomaticPackageResolution \
