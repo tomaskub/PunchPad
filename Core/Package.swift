45624cc75d0266b4828739df7e6a808375c09c9a
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2025-04-21 15:17:17 +0200

    CI fixes

--- Core/Package.swift
+++ Core/Package.swift
@@ -45,7 +45,8 @@ let package = Package(
         .target(
             name: "Persistance",
             dependencies: [
-                .target(name: "DomainModels")
+                "DomainModels",
+                "FoundationExtensions"
             ]
         )
     ]
--- Core/Sources/Persistance/PreviewDataManager.swift
+++ Core/Sources/Persistance/PreviewDataManager.swift
@@ -5,8 +5,8 @@
 //  Created by Tomasz Kubiak on 05/05/2024.
 //
 import DomainModels
-import UIComponents
 import Foundation
+import FoundationExtensions
 import Combine
 import OSLog
 
@@ -15,10 +15,14 @@ public final class PreviewDataManager: NSObject, DataManaging {
     private var data = [Entry]()
     private let logger = Logger.dataManager
     
-    public init(with initialData: [Entry] = PreviewDataFactory.buildDataForPreviewForYear()) {
+    public init(with initialData: [Entry]? = nil) {
         super.init()
         logger.debug("Initializing sata manager")
-        data = initialData
+        if let initialData {
+            data = initialData
+        } else {
+            data = buildDataForPreviewForYear()
+        }
         dataDidChange.send()
     }
     
@@ -87,5 +91,58 @@ public final class PreviewDataManager: NSObject, DataManaging {
     public func fetchNewestExisting() -> Entry? {
         logger.debug("fetchNewest called")
         return data.max { $0.startDate > $1.startDate }
+    }
+    
+    private  func buildDataForPreviewForYear(containing date: Date = Date(),
+                                             using calendar: Calendar = .current) -> [Entry] {
+        var result = [Entry]()
+        let year = calendar.dateComponents([.year], from: date).year!
+        for month in 1...12 {
+            guard let dateInMonth = calendar.date(from: DateComponents(year: year, month: month)) else { continue }
+            let dataForMonth = buildDataForPreviewForMonth(containing: dateInMonth, using: calendar)
+            result.append(contentsOf: dataForMonth)
+        }
+        return result
+    }
+    
+    private func buildDataForPreviewForMonth(containing date: Date = Date(),
+                                            using calendar: Calendar = Calendar.current) -> [Entry] {
+        let currentDate = calendar.startOfDay(for: date)
+        guard let numberOfDaysInMonth = calendar.range(of: .day, in: .month, for: currentDate) else { return [] }
+        let dateComponents = calendar.dateComponents([.year, .month], from: currentDate)
+        var result = [Entry]()
+        for day in numberOfDaysInMonth {
+            var localDateComponents = dateComponents
+            localDateComponents.day = day
+            guard let workingDate = calendar.date(from: localDateComponents),
+                  !calendar.isDateInWeekend(workingDate),
+                  let entry = buildDataForDay(for: workingDate, using: calendar) else { continue }
+            result.append(entry)
+        }
+        return result
+    }
+    
+    private func buildDataForDay(for dayDate: Date = Date(),
+                                        using calendar: Calendar = .current,
+                                        startDateVariation: ClosedRange<Int> = 0...4,
+                                        timeLengthVariation: ClosedRange<Int> = -4...4) -> Entry? {
+        let randomStartComponent = Int.random(in: startDateVariation)
+        let randomLengthComponent = Int.random(in: timeLengthVariation)
+        let workTime = randomLengthComponent < 0 ? (8 + randomLengthComponent) * 3600 : 8 * 3600
+        let overtime = randomLengthComponent <= 0 ? 0 : randomLengthComponent * 3600
+        guard let startDate = calendar.date(byAdding: .hour, value: 6 + randomStartComponent, to: dayDate),
+              let finishDate = calendar.date(byAdding: .hour, value: 8 + randomLengthComponent, to: startDate) else {
+            return nil
+        }
+        return Entry(
+            startDate: startDate,
+            finishDate: finishDate,
+            workTimeInSec: workTime,
+            overTimeInSec: overtime,
+            maximumOvertimeAllowedInSeconds: 5*3600,
+            standardWorktimeInSeconds: 8*3600,
+            grossPayPerMonth: 10000,
+            calculatedNetPay: nil
+        )
     }
 }
